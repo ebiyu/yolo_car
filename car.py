cf7f291db33ebeb801eaf2420ca52825b00311ea
Yusuke Ebihara <yusuke@ebihara.me>
2025-06-11 10:00:16 +0100

    add LED controller

--- car.py
+++ car.py
@@ -32,7 +32,14 @@ class Car:
         """
         l = int(l * 256 / 100)
         r = int(r * 256 / 100)
-        self.ser.write(str(r).encode() + b' ' + str(l).encode() + b'\n')
+        self.ser.write(b'M' + str(r).encode() + b' ' + str(l).encode() + b'\n')
+
+    def set_LED(self, r, g, b):
+        """
+        r, g, b: 0 - 255
+        """
+        self.ser.write(b'L' + str(r).encode() + b' ' + str(g).encode() + b' ' + str(b).encode() + b'\n')
+
 
     def close(self):
         self.ser.close()
@@ -40,13 +47,18 @@ class Car:
 if __name__ == "__main__":
     car = Car()
     
+    car.set_LED(255, 0, 0)
     car.move_forward(70)
     time.sleep(0.6)
+    car.set_LED(0, 255, 0)
     car.move_backward(70)
     time.sleep(0.6)
+    car.set_LED(0, 0, 255)
     car.turn_left(70)
     time.sleep(0.6)
+    car.set_LED(255, 255, 255)
     car.turn_right(70)
     time.sleep(0.6)
+    car.set_LED(0, 0, 0)
     car.stop()
     car.close()
--- yolo_car.ino
+++ yolo_car.ino
@@ -1,3 +1,8 @@
+// You have to install "Adafruit_NeoPixel" from Arduino library manager
+// cf. https://moyoi-memo.hatenablog.com/entry/2019/05/13/124031
+#include <Adafruit_NeoPixel.h>
+
+
 #define PIN_Key 2
 #define PIN_RBGLED 4
 
@@ -13,7 +18,7 @@
 void receiveI2CHandler();
 void onI2CRegSet(uint8_t addr, uint8_t val);
 
-//Servo myservo;
+Adafruit_NeoPixel led = Adafruit_NeoPixel(1, PIN_RBGLED, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   pinMode(PIN_Motor_STBY, OUTPUT);
@@ -21,13 +26,16 @@ void setup() {
   pinMode(PIN_Motor_AIN_1, OUTPUT);
   pinMode(PIN_Motor_PWMB, OUTPUT);
   pinMode(PIN_Motor_BIN_1, OUTPUT);
+  pinMode(PIN_RBGLED, OUTPUT);
 
+  
   digitalWrite(PIN_Motor_STBY, HIGH);
   digitalWrite(PIN_Motor_PWMA, LOW);
   digitalWrite(PIN_Motor_AIN_1, LOW);
   digitalWrite(PIN_Motor_PWMB, LOW);
   digitalWrite(PIN_Motor_BIN_1, LOW);
 
+  led.begin();
 //  myservo.attach(PIN_Servo_1);
 
   Serial.begin(9600);
@@ -37,6 +45,7 @@ void setup() {
 constexpr int BUF_SIZE = 256;
 char buf[BUF_SIZE];
 int buf_ptr = 0;
+int text_len = 0;
 char send_buf[255];
 
 void loop() {
@@ -45,37 +54,52 @@ void loop() {
         Serial.write(c);
         if (c == '\n' || c == '\r') {
             buf[buf_ptr] = '\0';
+            text_len = buf_ptr;
             buf_ptr = 0;
-            int l, r;
-            int n = sscanf(buf, "%d %d", &l, &r);
-            if (n != 2) return;
 
-            l = constrain(l, -255, 255);
-            r = constrain(r, -255, 255);
+            if (text_len <= 1) return;
 
-            if (l == 0) {
-                digitalWrite(PIN_Motor_PWMA, LOW);
-            } else if (l > 0) {
-                digitalWrite(PIN_Motor_AIN_1, HIGH);
-                analogWrite(PIN_Motor_PWMA, l);
-            } else {
-                digitalWrite(PIN_Motor_AIN_1, LOW);
-                analogWrite(PIN_Motor_PWMA, -l);
+            if (buf[0] == 'M') {
+              // motor control
+              int l, r;
+              int n = sscanf(buf + 1, "%d %d", &l, &r);
+              if (n != 2) return;
+  
+              l = constrain(l, -255, 255);
+              r = constrain(r, -255, 255);
+  
+              if (l == 0) {
+                  digitalWrite(PIN_Motor_PWMA, LOW);
+              } else if (l > 0) {
+                  digitalWrite(PIN_Motor_AIN_1, HIGH);
+                  analogWrite(PIN_Motor_PWMA, l);
+              } else {
+                  digitalWrite(PIN_Motor_AIN_1, LOW);
+                  analogWrite(PIN_Motor_PWMA, -l);
+              }
+  
+              if (r == 0) {
+                  digitalWrite(PIN_Motor_PWMB, LOW);
+              } else if (r > 0) {
+                  digitalWrite(PIN_Motor_BIN_1, HIGH);
+                  analogWrite(PIN_Motor_PWMB, r);
+              } else {
+                  digitalWrite(PIN_Motor_BIN_1, LOW);
+                  analogWrite(PIN_Motor_PWMB, -r);
+              }
+            } else if (buf[0] == 'L') {
+              // led control
+              int r, g, b;
+              int n = sscanf(buf + 1, "%d %d %d", &r, &g, &b);
+              if (n != 3) return;
+  
+              g = constrain(g, -255, 255);
+              r = constrain(r, -255, 255);
+              b = constrain(b, -255, 255);
+
+              led.setPixelColor(0, led.Color(g, r, b));
+              led.show();
             }
-
-            if (r == 0) {
-                digitalWrite(PIN_Motor_PWMB, LOW);
-            } else if (r > 0) {
-                digitalWrite(PIN_Motor_BIN_1, HIGH);
-                analogWrite(PIN_Motor_PWMB, r);
-            } else {
-                digitalWrite(PIN_Motor_BIN_1, LOW);
-                analogWrite(PIN_Motor_PWMB, -r);
-            }
-
-//            myservo.write(cam); 
-
-            // handle input
         } else {
             buf[buf_ptr] = c;
             buf_ptr++;
